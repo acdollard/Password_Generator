52a177e303fded8a7a97bf9c72d114c880b2f0a3
Alex Dollard <alexdollard141@gmail.com>
2019-11-07 14:45:28 -0600

    added if-statements linking user responses to appropriate arrays

--- script.js
+++ script.js
@@ -48,13 +48,45 @@ var askspecs = confirm("Do you want special characters in your password?");
 var askups = confirm("Do you want uppercase letters in your password?");
 var asklows = confirm("Do you want lowercase letters in your password?");
 
-//if statements linking user responses to appropriate string//
+//if statements linking user responses to appropriate array//
+if(asknums == true && askspecs == false && askups == false && asklows == false){
+    appropriateArray = nums
+} else if(asknums == false && askspecs == true && askups == false && asklows == false){
+    appropriateArray = specs
+} else if(asknums == false && askspecs == false && askups == true && asklows == false){
+    appropriateArray = ups
+} else if(asknums == false && askspecs == false && askups == false && asklows == true){
+    appropriateArray = lows
+} else if(asknums == true && askspecs == true && askups == false && asklows == false){
+    appropriateArray = numsSpecs
+} else if(asknums == false && askspecs == true && askups == false && asklows == true){
+    appropriateArray = specsLows
+} else if(asknums == false && askspecs == false && askups == true && asklows == true){
+    appropriateArray = lowsUps
+} else if(asknums == true && askspecs == false && askups == false && asklows == true){
+    appropriateArray = numsLows
+} else if(asknums == true && askspecs == false && askups == true && asklows == false){
+    appropriateArray = numsUps
+} else if(asknums == false && askspecs == true && askups == true && asklows == false){
+    appropriateArray = specsUps
+} else if(asknums == true && askspecs == true && askups == false && asklows == true){
+    appropriateArray = numsSpecsLows
+} else if(asknums == false && askspecs == true && askups == true && asklows == true){
+    appropriateArray = specsLowsUps
+} else if(asknums == true && askspecs == false && askups == true && asklows == true){
+    appropriateArray = numsLowsUps
+} else if(asknums == true && askspecs == true && askups == true && asklows == false){
+    appropriateArray = numsSpecsUps
+} else if(asknums == true && askspecs == true && askups == true && asklows == true){
+    appropriateArray = numbsSpecsUpsLows
+}
+
 
 //test//
    function GeneratePassword(){
         let password = "";
         for(let i=0; i<length; i++){
-        password = password + specs[Math.floor(Math.random() * specs.length)];
+        password = password + appropriateArray[Math.floor(Math.random() * appropriateArray.length)];
         };
         return password;
     }
